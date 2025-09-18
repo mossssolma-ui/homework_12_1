8db8c301f24cb686fcc7b3119f9be66ea17bddd9
Oleg Mat <mos_olma@mail.ru>
2025-09-18 13:34:58 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,3 +2,9 @@ def filter_by_state(lst: list, state: str = "EXECUTED") -> list:
     """Функция отбора по ключу state"""
     new_list = [i for i in lst if i.get("state") == state]
     return new_list
+
+
+def sort_by_date(lst: list, key_sort: bool = True) -> list:
+    """Функция сортировки даты"""
+    new_list = sorted(lst, key=lambda x: x.get("date"), reverse=key_sort)
+    return new_list
