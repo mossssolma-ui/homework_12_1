5205586e1973b8801e81630a3829503f2d737fba
Oleg Mat <mos_olma@mail.ru>
2025-10-07 19:50:22 +0300

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -19,3 +19,16 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[str | None, 
     """
     for transaction in transactions:
         yield transaction.get("description")
+
+
+def card_number_generator(start: int, end: int) -> Generator[str, Any, None]:
+    """
+    Функция генерации номеров банковских карт в диапазоне
+    от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Принимает начальное и конечное значения.
+    """
+    card_number = "0" * 16
+    for num in range(start, end + 1):
+        card_number = card_number[: -len(str(num))] + str(num)
+        card_number_format = " ".join(card_number[i: i + 4] for i in range(0, 16, 4))
+        yield card_number_format
